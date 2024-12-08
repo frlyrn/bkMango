1a266b3fa22676fd284b78f1ece8fc94da239643
fauraalyriana <fauraalyrianas@gmail.com>
2024-12-08 21:50:33 +0700

    second commit

--- src/server/server.js
+++ src/server/server.js
@@ -2,25 +2,37 @@ require('dotenv').config();
  
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
-
+const inputError = require('../exceptions/inputError');
+ 
 (async () => {
     const server = Hapi.server({
-        port: 8080,
-        host: '0.0.0.0',
+        port: 3000,
+        host: 'localhost',
         routes: {
             cors: {
               origin: ['*'],
             },
+            payload: {
+                maxBytes: 1000000, 
+            },
         },
-    })
+    });
  
     server.route(routes);
  
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
     
+        if (response instanceof inputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message} Silakan gunakan foto lain.`
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+    
         if (response.isBoom) {
-            console.error('Error occurred:', response.message); // Tambahkan ini untuk log error
             if (response.output.statusCode === 413) {
                 const newResponse = h.response({
                     status: 'fail',
@@ -31,16 +43,16 @@ const routes = require('../server/routes');
             }
     
             const newResponse = h.response({
-                status: 'error',
-                message: 'Internal server error',
+                status: 'fail',
+                message: response.message,
             });
             newResponse.code(response.output.statusCode);
             return newResponse;
         }
     
         return h.continue;
-    });
-    
+    });    
+ 
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
