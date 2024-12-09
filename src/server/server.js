880b0b054fa98a9309e53ff7a611053a62ecd8e9
fauraalyriana <fauraalyrianas@gmail.com>
2024-12-09 09:53:22 +0700

    edit server

--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,31 @@
 require('dotenv').config();
- 
+
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const inputError = require('../exceptions/inputError');
- 
+
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: 8080,
         host: '0.0.0.0',
         routes: {
             cors: {
-              origin: ['*'],
+                origin: ['*'],
+                headers: ['Accept', 'Content-Type', 'Authorization'], 
+                exposedHeaders: ['Authorization'], 
+                credentials: true,
             },
             payload: {
-                maxBytes: 1000000, 
+                maxBytes: 1000000,
             },
         },
     });
- 
+
     server.route(routes);
- 
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
-    
+
         if (response instanceof inputError) {
             const newResponse = h.response({
                 status: 'fail',
@@ -31,7 +34,7 @@ const inputError = require('../exceptions/inputError');
             newResponse.code(response.statusCode);
             return newResponse;
         }
-    
+
         if (response.isBoom) {
             if (response.output.statusCode === 413) {
                 const newResponse = h.response({
@@ -41,7 +44,7 @@ const inputError = require('../exceptions/inputError');
                 newResponse.code(413);
                 return newResponse;
             }
-    
+
             const newResponse = h.response({
                 status: 'fail',
                 message: response.message,
@@ -49,10 +52,10 @@ const inputError = require('../exceptions/inputError');
             newResponse.code(response.output.statusCode);
             return newResponse;
         }
-    
+
         return h.continue;
-    });    
- 
+    });
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
